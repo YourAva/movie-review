5657232a1561ff6070e1762ec87a493e8809a021
AvaLikesBread <dcut2171@gmail.com>
2024-01-30 18:08:04 +0000

    Giant update -- Navbar, Carousel

--- src/App.js
+++ src/App.js
@@ -1,98 +1,140 @@
 import "./App.css";
-
-const movieData = [
-  {
-    title: "Inception",
-    description:
-      "A thief who enters the dreams of others to steal their secrets from their subconscious.",
-    reviewAmount: "87",
-    director: "Christopher Nolan",
-    releaseYear: 2010,
-    starring: ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Ellen Page"],
-    genre: "Action",
-    runtime: "148 minutes",
-    image: "./inception.jpg",
-  },
-  {
-    title: "The Shawshank Redemption",
-    description:
-      "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
-    reviewAmount: "91",
-    director: "Frank Darabont",
-    releaseYear: 1994,
-    starring: ["Tim Robbins", "Morgan Freeman"],
-    genre: "Drama",
-    runtime: "142 minutes",
-    image: "./shawshank.jpg",
-  },
-  {
-    title: "The Godfather",
-    description:
-      "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
-    reviewAmount: "98",
-    director: "Francis Ford Coppola",
-    releaseYear: 1972,
-    starring: ["Marlon Brando", "Al Pacino", "James Caan"],
-    genre: "Crime",
-    runtime: "175 minutes",
-    image: "./godfather.jpg",
-  },
-  {
-    title: "Forrest Gump",
-    description:
-      "The presidencies of Kennedy and Johnson, the Vietnam War, the Watergate scandal and other historical events unfold from the perspective of an Alabama man with an IQ of 75, whose only desire is to be reunited with his childhood sweetheart.",
-    reviewAmount: "71",
-    director: "Robert Zemeckis",
-    releaseYear: 1994,
-    starring: ["Tom Hanks", "Robin Wright", "Gary Sinise"],
-    genre: "Drama",
-    runtime: "142 minutes",
-    image: "./forrest-gump.jpg",
-  },
-];
+import movieData from "./movies.js";
+import React, { useState } from "react";
 
 function App() {
+  const carouselItems = [
+    {
+      imgAlt: "Item 1",
+      img: "./movies/matrix.jpg",
+    },
+    {
+      imgAlt: "Item 2",
+      img: "./movies/fightclub.jpg",
+    },
+    {
+      imgAlt: "Item 3",
+      img: "./movies/darkKnight.jpg",
+    },
+  ];
   const movies = movieData;
   const numMovies = movies.length;
 
   return (
     <div className="App">
+      <Navbar />
+      <Carousel items={carouselItems} />
       {numMovies > 0 && (
-        <ul className="movies">
+        <div className="movies">
           {movieData.map((movie) => (
             <Movie movieObj={movie} key={movie.title} />
           ))}
-        </ul>
+        </div>
       )}
     </div>
   );
 }
 
+function Navbar() {
+  return (
+    <div className="navbar">
+      <h1>React Movies</h1>
+    </div>
+  );
+}
+
+const Carousel = ({ items }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToPreviousSlide = () => {
+    const index = (currentIndex - 1 + items.length) % items.length;
+    setCurrentIndex(index);
+  };
+
+  const goToNextSlide = () => {
+    const index = (currentIndex + 1) % items.length;
+    setCurrentIndex(index);
+  };
+
+  return (
+    <div className="carousel">
+      <div className="carousel-items">
+        {items.map((item, index) => (
+          <div
+            key={index}
+            className={index === currentIndex ? "active" : "inactive"}
+          >
+            <img
+              src={item.img}
+              alt={item.imgAlt}
+              className="carousel-items-image"
+            />
+          </div>
+        ))}
+      </div>
+      <button onClick={goToPreviousSlide} className="carousel-button prev">
+        <img
+          src="./arrow-left.png"
+          alt="arrow-left"
+          className="carousel-button-left-arrow"
+        />
+      </button>
+      <button onClick={goToNextSlide} className="carousel-button next">
+        <img
+          src="./arrow-right.png"
+          alt="arrow-right"
+          className="carousel-button-right-arrow"
+        />
+      </button>
+    </div>
+  );
+};
+
 function Movie(props) {
   return (
-    <li className="movie">
-      <img src={props.movieObj.image} className="movie-cover" alt={props.movieObj.title} />
+    <div className="movie">
+      <img
+        src={props.movieObj.image}
+        className="movie-cover"
+        alt={props.movieObj.title}
+      />
       <div className="movie-titles">
         <h2>{props.movieObj.title}</h2>
         <p>{props.movieObj.director}</p>
       </div>
       <div className="movie-data">
         <div className="movie-review">
-          <img src="./rottenTomatoes.png" className="movie-reviews-tomato" alt="Rotten Tomatoes" />
+          <img
+            src="./rottenTomatoes.png"
+            className="movie-reviews-tomato"
+            alt="Rotten Tomatoes"
+          />
           <p>{props.movieObj.reviewAmount}%</p>
         </div>
         <div className="movie-description">
-          <p>{props.movieObj.title} was made in {props.movieObj.releaseYear}, with the genre of {props.movieObj.genre}.</p>
+          <ul>
+            <li>
+              <b>Genre:</b> {props.movieObj.genre}
+            </li>
+            <li>
+              <b>Released:</b> {props.movieObj.releaseYear}
+            </li>
+            <li>
+              <b>Runtime:</b> {props.movieObj.runtime}
+            </li>
+          </ul>
         </div>
       </div>
       <div className="rating">
         <div className="rating-bar">
-          <div className="rating-bar-progress" style={{width: `${props.movieObj.reviewAmount}%`}}></div>
+          <div
+            className="rating-bar-progress"
+            style={{ width: `${props.movieObj.reviewAmount}%` }}
+          ></div>
         </div>
       </div>
-    </li>
+    </div>
   );
 }
-
 
 export default App;
---
+++ src/movies.js
@@ -0,0 +1,124 @@
+const movieData = [
+  {
+    title: "Inception",
+    description:
+      "A thief who enters the dreams of others to steal their secrets from their subconscious.",
+    reviewAmount: "87",
+    director: "Christopher Nolan",
+    releaseYear: 2010,
+    starring: ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Ellen Page"],
+    genre: "Action",
+    runtime: "148 minutes",
+    image: "./movies/inception.jpg",
+  },
+  {
+    title: "The Shawshank Redemption",
+    description:
+      "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+    reviewAmount: "91",
+    director: "Frank Darabont",
+    releaseYear: 1994,
+    starring: ["Tim Robbins", "Morgan Freeman"],
+    genre: "Drama",
+    runtime: "142 minutes",
+    image: "./movies/shawshank.jpg",
+  },
+  {
+    title: "The Godfather",
+    description:
+      "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+    reviewAmount: "98",
+    director: "Francis Ford Coppola",
+    releaseYear: 1972,
+    starring: ["Marlon Brando", "Al Pacino", "James Caan"],
+    genre: "Crime",
+    runtime: "175 minutes",
+    image: "./movies/theGodfather.jpg",
+  },
+  {
+    title: "Forrest Gump",
+    description:
+      "The presidencies of Kennedy and Johnson, the Vietnam War, the Watergate scandal and other historical events unfold from the perspective of an Alabama man with an IQ of 75, whose only desire is to be reunited with his childhood sweetheart.",
+    reviewAmount: "71",
+    director: "Robert Zemeckis",
+    releaseYear: 1994,
+    starring: ["Tom Hanks", "Robin Wright", "Gary Sinise"],
+    genre: "Drama",
+    runtime: "142 minutes",
+    image: "./movies/forrestGump.jpg",
+  },
+  {
+    title: "The Dark Knight",
+    description:
+      "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+    reviewAmount: "94",
+    director: "Christopher Nolan",
+    releaseYear: 2008,
+    starring: ["Christian Bale", "Heath Ledger", "Aaron Eckhart"],
+    genre: "Action",
+    runtime: "152 minutes",
+    image: "./movies/darkKnight.jpg",
+  },
+  {
+    title: "Pulp Fiction",
+    description:
+      "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+    reviewAmount: "94",
+    director: "Quentin Tarantino",
+    releaseYear: 1994,
+    starring: ["John Travolta", "Uma Thurman", "Samuel L. Jackson"],
+    genre: "Crime",
+    runtime: "154 minutes",
+    image: "./movies/pulpFiction.jpg",
+  },
+  {
+    title: "The Matrix",
+    description:
+      "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+    reviewAmount: "87",
+    director: "Lana Wachowski, Lilly Wachowski",
+    releaseYear: 1999,
+    starring: ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss"],
+    genre: "Action",
+    runtime: "136 minutes",
+    image: "./movies/matrix.jpg",
+  },
+  {
+    title: "Schindler's List",
+    description:
+      "In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.",
+    reviewAmount: "93",
+    director: "Steven Spielberg",
+    releaseYear: 1993,
+    starring: ["Liam Neeson", "Ben Kingsley", "Ralph Fiennes"],
+    genre: "Biography",
+    runtime: "195 minutes",
+    image: "./movies/schnidersList.jpeg",
+  },
+  {
+    title: "Fight Club",
+    description:
+      "An insomniac office worker and a devil-may-care soapmaker form an underground fight club that evolves into something much, much more.",
+    reviewAmount: "79",
+    director: "David Fincher",
+    releaseYear: 1999,
+    starring: ["Brad Pitt", "Edward Norton", "Helena Bonham Carter"],
+    genre: "Drama",
+    runtime: "139 minutes",
+    image: "./movies/fightClub.jpg",
+  },
+  {
+    title: "The Lord of the Rings: The Fellowship of the Ring",
+    description:
+      "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.",
+    reviewAmount: "91",
+    director: "Peter Jackson",
+    releaseYear: 2001,
+    starring: ["Elijah Wood", "Ian McKellen", "Orlando Bloom"],
+    genre: "Adventure",
+    runtime: "178 minutes",
+    image: "./movies/lotr.jpg",
+  },
+];
+
+export default movieData;
